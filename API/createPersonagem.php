666d75895bb99bdeab7f66030a8129ab7f0f02e9
Gabriel Nori <gabrielnori@gabrielnori.tech>
2020-08-09 10:56:11 -0300

    .

--- API/createPersonagem.php
+++ API/createPersonagem.php
@@ -1,6 +1,8 @@
 <?php
 	//require "./DB/connection.php";
 	$conexao = mysqli_connect("localhost", "u179156626_admin", ">gXuR1g9xS", "u179156626_personagem") or die ("A conexão não foi executada com sucesso");
+	
+	header('Content-Type: application/javascript; charset=utf8');
 
 	$queryGetPele = "SELECT * FROM pele";
 	$queryGetOlhos = "SELECT * FROM olhos";
@@ -9,21 +11,26 @@
 	$queryGetNariz = "SELECT * FROM nariz";
 	$queryGetMake = "SELECT * FROM make";
 	$queryGetDetalhe = "SELECT * FROM detalhe";
-	
-	header('Content-Type: application/javascript; charset=utf8');
+	$peles = "";
+	$olhos = "";
+	$bocas = "";
+	$cabelos = "";
+	$narizes = "";
+	$makes = "";
+	$detalhes = "";
+	$qtdDetalhes = 2;
 
 	if(isset($_POST['qtdDetalhes'])){
 		$qtdDetalhes = $_POST['qtdDetalhes'];
 	}
 
-
-	$peles = mysqli_fetch_array(mysqli_query($conexao, $queryGetPele));
-	$olhos = mysqli_fetch_array(mysqli_query($conexao, $queryGetOlhos));
-	$bocas = mysqli_fetch_array(mysqli_query($conexao, $queryGetBoca));
-	$cabelos = mysqli_fetch_array(mysqli_query($conexao, $queryGetCabelo));
-	$narizes = mysqli_fetch_array(mysqli_query($conexao, $queryGetNariz));
-	$makes = mysqli_fetch_array(mysqli_query($conexao, $queryGetMake));
-	$detalhes = mysqli_fetch_array(mysqli_query($conexao, $queryGetDetalhe));
+	$peles = mysqli_fetch_all(mysqli_query($conexao, $queryGetPele));
+	$olhos = mysqli_fetch_all(mysqli_query($conexao, $queryGetOlhos));
+	$bocas = mysqli_fetch_all(mysqli_query($conexao, $queryGetBoca));
+	$cabelos = mysqli_fetch_all(mysqli_query($conexao, $queryGetCabelo));
+	$narizes = mysqli_fetch_all(mysqli_query($conexao, $queryGetNariz));
+	$makes = mysqli_fetch_all(mysqli_query($conexao, $queryGetMake));
+	$detalhes = mysqli_fetch_all(mysqli_query($conexao, $queryGetDetalhe));
 
 	function insertPersonagem(){
 		$pele = $peles[rand(0,sizeof($peles))];
