9e6ab871cf7a3221e9936986b1047a904de7d8e6
Gabriel Nori <gabrielnori@gabrielnori.tech>
2020-08-09 10:29:07 -0300

    HARDCODED

--- API/DB/connection.php
+++ API/DB/connection.php
@@ -1,4 +1,5 @@
 <?php
-	require "../credentials.php";
-	$conexao = mysqli_connect($servername, $username, $password, $dbname) or die ("A conexão não foi executada com sucesso");
+	//require "../credentials.php";
+	//$conexao = mysqli_connect($servername, $username, $password, $dbname) or die ("A conexão não foi executada com sucesso");
+	$conexao = mysqli_connect("localhost", "u179156626_admin", ">gXuR1g9xS", "u179156626_personagem") or die ("A conexão não foi executada com sucesso");
 ?>
